107485487c5279e16e5dd771027d0ad9fe04bb5b
mozu888 <94517488+mozu888@users.noreply.github.com>
2022-01-12 14:36:14 +0900

    taskテーブルのマイグレーションの設定

--- db/migrate/20220112053136_create_tasks.rb
+++ db/migrate/20220112053136_create_tasks.rb
@@ -1,6 +1,9 @@
 class CreateTasks < ActiveRecord::Migration[6.1]
   def change
     create_table :tasks do |t|
+      t.string :title, null: false
+      t.text :text, null: false
+      t.date :date, null: false
 
       t.timestamps
     end
