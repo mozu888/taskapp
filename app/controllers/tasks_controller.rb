571819f515faba0ec37d9bd858ccb369bbab15ee
mozu888 <94517488+mozu888@users.noreply.github.com>
2022-01-12 16:39:44 +0900

    newとcreateアクションの設定

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,18 @@ class TasksController < ApplicationController
   def index
   end
   def new
+    @task = Task.new
   end
   def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+  private
+  def task_params
+    params.require(:task).permit(:title, :text, :date)
   end
 end
